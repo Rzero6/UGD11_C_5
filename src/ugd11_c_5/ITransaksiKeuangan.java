e55239f9d8dfa03e2bf20b0895f2ea3bfee8052d
Reynoldssss <michaelreynoldk@gmail.com>
2023-05-19 16:41:46 +0700

    Merge origin/master

---
+++ src/ugd11_c_5/ITransaksiKeuangan.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ugd11_c_5;
+
+/**
+ *
+ * @author VICTUS
+ */
+public interface ITransaksiKeuangan {
+    public void TransaksiKeuangan();
+}
